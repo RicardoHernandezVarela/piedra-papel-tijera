50c292427e1f0bf7e62b4d7b514865dfbea154b1
RicardoHernandezVarela <rhvarela011192@gmail.com>
2020-01-27 15:05:30 -0600

    Computadora puede seleccionar entre opciones del juego

--- js/app.js
+++ js/app.js
@@ -1,13 +1,54 @@
+/* DOM ELEMENTS */
 const playerOptions = document.querySelector('#player-options');
 const playerSelection = document.querySelector('#player-selection');
+const computerSelection = document.querySelector('#computer-selection');
 
+/* GAME OPTIONS */
+const gameOptions = [
+    {
+        choice: "papel"
+    },
+    {
+        choice: "piedra"
+    },
+    {
+        choice: "tijeras"
+    }
+]
+
+/* TURNS */
+let playerCanPlay = true;
+
+/* GAME CONDITIONS */
+
+
+/* GENERATE MOVE */
+const generateMove = (choice) => {
+    let move = `
+        <span class="sprite ${choice}"></span>
+        <h5>${choice}</h5>
+    `;
+    return move;
+}
+
+/* COMPUTER CHOICE*/
+const computerChoice = () => {
+    selected = Math.floor(Math.random()*3);
+    computerMove = gameOptions[selected].choice;
+    computerSelection.innerHTML = generateMove(computerMove);
+}
+
+
+/* USER CHOICE */
 playerOptions.addEventListener('click', (event) => {
     let playerChoice = event.target.id;
     
-    let move = `
-        <span class="sprite ${playerChoice}"></span>
-        <h5>${playerChoice}</h5>
-    `;
+    /* PLAYER MOVE */
+    playerSelection.innerHTML = generateMove(playerChoice);
+    playerCanPlay = false;
 
-    playerSelection.innerHTML = move;
+    /* COMPUTER MOVE */
+    if(!playerCanPlay) {
+        computerChoice();
+    }
 })
